4b0a79e45a1bfefc18f1429ecf9c7067d7caad13
Eduardo Tenório de Souza <esouza@stone.com.br>
2017-08-11 20:07:12 -0300

    Ajuste no código fonte.

--- RabbitMQClient/Entity/Producer.cs
+++ RabbitMQClient/Entity/Producer.cs
@@ -3,33 +3,46 @@ using System.Text;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 
-namespace RabbitMQClient.Entity
-{
-    public class Producer<T> : IDisposable
-    {
+namespace RabbitMQClient.Entity {
+    /// <summary>
+    /// Responsável por produzir mensagens na fila
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Producer<T> : IDisposable {
+        /// <summary>
+        /// Canal de comunicação com a fila
+        /// </summary>
         private readonly IModel _channel;
+
+        /// <summary>
+        /// Descrição da fila
+        /// </summary>
         private readonly string _queueName;
 
-        public Producer(IModel channel, string queueName)
-        {
+        /// <summary>
+        /// Método construtor parametrizado
+        /// </summary>
+        /// <param name="channel">Canal de comunicação com a fila</param>
+        /// <param name="queueName">Descrição da fila</param>
+        public Producer(IModel channel, string queueName) {
             this._channel = channel;
             this._queueName = queueName;
         }
 
-        public void Publish(T obj)
-        {
+        /// <summary>
+        /// Publica a mensagem na fila
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Publish(T obj) {
             var data = JsonConvert.SerializeObject(obj);
-
             var buffer = Encoding.UTF8.GetBytes(data);
-
             _channel.BasicPublish(exchange: "", routingKey: _queueName, basicProperties: null, body: buffer);
         }
 
         /// <summary>
         /// 'IDisposable' implementation.
         /// </summary>
-        public void Dispose()
-        {
+        public void Dispose() {
             this.Dispose(true);
             GC.SuppressFinalize(this);
         }
@@ -38,14 +51,12 @@ namespace RabbitMQClient.Entity
         /// 'IDisposable' implementation.
         /// </summary>
         /// <param name="disposeManaged">Whether to dispose managed resources.</param>
-        protected virtual void Dispose(bool disposeManaged)
-        {
+        protected virtual void Dispose(bool disposeManaged) {
             // Return if already disposed.
             if (this._alreadyDisposed) return;
 
             // Release managed resources if needed.
-            if (disposeManaged)
-            {
+            if (disposeManaged) {
                 this._channel.Dispose();
             }
 
--- RabbitMQClient/QueueManager.cs
+++ RabbitMQClient/QueueManager.cs
@@ -3,34 +3,76 @@ using RabbitMQ.Client;
 using RabbitMQClient.Entity;
 using RabbitMQClient.Library;
 
-namespace RabbitMQClient
-{
-    public class QueueManager<T> : IDisposable
-    {
+namespace RabbitMQClient {
+
+    /// <summary>
+    /// Responsável por gerenciar publicação e o consumo no RabbitMQ
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QueueManager<T> : IDisposable {
+
+        /// <summary>
+        /// Canal de comunicação com a fila
+        /// </summary>
         private IModel _channel;
+
+        /// <summary>
+        /// Produtor de mensagens
+        /// </summary>
         public Producer<T> Producer { get; set; }
+
+        /// <summary>
+        /// Consumidor de mensagens
+        /// </summary>
         public Consumer<T> Consumer { get; set; }
+
+        /// <summary>
+        /// Descrição da fila
+        /// </summary>
         private readonly string _queueName;
 
-        public QueueManager(string queueName)
-        {
+        /// <summary>
+        /// Método construtor parametrizado
+        /// </summary>
+        /// <param name="queueName">Descrição da fila</param>
+        public QueueManager(string queueName) {
             _queueName = queueName;
         }
 
-        public QueueManager<T> WithConnectionSetting(ConnectionSetting connectionSetting)
-        {
-            _channel = ChannelFactory.Create(connectionSetting);
-
+        /// <summary>
+        /// Atribui uma configuração para conexão com RabbitMQ
+        /// </summary>
+        /// <param name="connectionSetting">Configurações de conexão</param>
+        /// <returns>Instância de gerenciamento de fila com uma conexão com RabbitMQ</returns>
+        public QueueManager<T> WithConnectionSetting(ConnectionSetting connectionSetting) {
+            CreateChannel(connectionSetting);
             return this;
         }
 
+        /// <summary>
+        /// Cria uma canal de comunicação com RabbitMQ
+        /// </summary>
+        /// <param name="connectionSetting"></param>
+        private void CreateChannel(ConnectionSetting connectionSetting) {
+            _channel = ChannelFactory.Create(connectionSetting);
+        }
+
+        /// <summary>
+        /// Atribui um consumidor ao gerenciador 
+        /// </summary>
+        /// <param name="prefetchCount">Controla o número de mensagem recebidas</param>
+        /// <param name="autoAck">Indica se a mensagem será removida ou não da fila</param>
+        /// <returns>Instância de gerenciamento de fila com um consumidor</returns>
         public QueueManager<T> WithConsumer(ushort prefetchCount = 1, bool autoAck = false) {
 
             this.Consumer = new Consumer<T>(_channel, _queueName, prefetchCount, autoAck);
-
             return this;
         }
 
+        /// <summary>
+        /// Atribui um produtor ao gerenciador
+        /// </summary>
+        /// <returns>Instância de gerenciamento de fila com um produtor</returns>
         public QueueManager<T> WithProducer() {
 
             this.Producer = new Producer<T>(_channel, _queueName);
@@ -40,8 +82,7 @@ namespace RabbitMQClient
         /// <summary>
         /// 'IDisposable' implementation.
         /// </summary>
-        public void Dispose()
-        {
+        public void Dispose() {
             this.Dispose(true);
             GC.SuppressFinalize(this);
         }
@@ -50,14 +91,12 @@ namespace RabbitMQClient
         /// 'IDisposable' implementation.
         /// </summary>
         /// <param name="disposeManaged">Whether to dispose managed resources.</param>
-        protected virtual void Dispose(bool disposeManaged)
-        {
+        protected virtual void Dispose(bool disposeManaged) {
             // Return if already disposed.
             if (this._alreadyDisposed) return;
 
             // Release managed resources if needed.
-            if (disposeManaged)
-            {
+            if (disposeManaged) {
                 this.Consumer?.Dispose();
                 this.Producer?.Dispose();
                 this._channel?.Dispose();
